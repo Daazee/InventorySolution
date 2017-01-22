38bc1a917a662f05509f41505b7e046bde6b984f
Daazee <azeez.adedayo@yahoo.com>
2017-01-23 00:09:56 +0100

    Added manage Users controller and changed app index page

---
+++ Inventory.Web/Areas/Admin/Controllers/ManageUserController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Inventory.Model;
+using Inventory.BLL;
+
+namespace Inventory.Web.Areas.Admin.Controllers
+{
+    public class ManageUserController : Controller
+    {
+        UserBs userBs = new UserBs();
+        // GET: Admin/ManageUser
+        public ActionResult ListUsers()
+        {
+            try
+            {
+                ViewBag.UserId = Session["Username"].ToString();
+            }
+            catch
+            {
+                Session["ConfirmLogin"] = "You must login first";
+                return RedirectToAction("Login", new { Area = "Security", Controller = "Access" });
+            }
+
+            return View(userBs.ListAll());
+        }
+
+        public ActionResult UpdateUser(string username)
+        {
+            try
+            {
+                ViewBag.UserId = Session["Username"].ToString();
+            }
+            catch
+            {
+                Session["ConfirmLogin"] = "You must login first";
+                return RedirectToAction("Login", new { Area = "Security", Controller = "Access" });
+            }
+
+            return View(userBs.GetByUsername(username));
+        }
+
+    }
+}
